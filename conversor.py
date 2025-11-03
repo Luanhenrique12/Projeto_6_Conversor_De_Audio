48647c5e210487bc05c28590c3b8617b5d20f20b
Luanhenrique12 <hluan8867@gmail.com>
2025-11-03 15:13:43 -0300

    Convertendo arquivo txt para mp3
    conversor de audio

---
+++ conversor.py
@@ -0,0 +1,16 @@
+def lê_binário():
+    try:
+        with open("audio_TOP_SECRET.txt", "rb") as fs1: 
+            dados = fs1.read()
+            print(type(dados))
+        with open("convertido.mp3", "wb") as fs2: 
+            fs2.write(dados)
+    except FileNotFoundError as e:
+        print('Arquivo não existe! -_-|||', e)
+        arquivo = open("audio.mp3", "wb")
+        arquivo.close()
+    except IOError as e:
+        print('Deu algo errado @_@')
+    print("OK! ~_~")
+
+lê_binário()
